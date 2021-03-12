7a18d9b069399039537c6399d0369fee73989cfb
deno750 <deronjic.denis@gmail.com>
2021-03-12 12:46:25 +0100

    x_pos function started implementation. Added the saving lp model file

--- include/solver.h
+++ include/solver.h
@@ -10,12 +10,12 @@
 
 static void build_model(instance *inst, CPXENVptr env, CPXLPptr lp);
 static int x_pos(int i, int j, int num_nodes);
-static double distance(int i, int j, instance *inst);
+static double calc_dist(int i, int j, instance *inst);
 
 int TSP_opt(instance *inst) {
     int error;
     CPXENVptr env = CPXopenCPLEX(&error);
-    CPXLPptr lp = CPXcreateprob(env, &error, "TSP");
+    CPXLPptr lp = CPXcreateprob(env, &error, inst->name);
     
     build_model(inst, env, lp);
 
@@ -35,8 +35,8 @@ int TSP_opt(instance *inst) {
 static void build_model(instance *inst, CPXENVptr env, CPXLPptr lp) {
     
     char xctype = 'B';
-    char **colname = (char *) calloc(1 , sizeof(char*)); // Cplex wants an array of variable names (i.e. char array of array)
-    colname[0] = (char) calloc(100, sizeof(char));
+    char **colname = (char **) calloc(1 , sizeof(char*)); // Cplex wants an array of variable names (i.e. char array of array)
+    colname[0] = (char *) calloc(100, sizeof(char));
 
 
     // We add one variable at time. We may also add them all in a single shot.
@@ -44,37 +44,40 @@ static void build_model(instance *inst, CPXENVptr env, CPXLPptr lp) {
 
         for (int j = i+1; j < inst->num_nodes; j++) {
             
-            sprintf(colname, "x(%d, %d)", i+1, j+1);
+            sprintf(colname[0], "x(%d,%d)", i+1, j+1);
 
             // Variables treated as single value arrays.
-            double obj = distance(i, j, inst); 
+            double obj = calc_dist(i, j, inst); 
             double lb = 0.0;
             double ub = 1.0;
 
             int status = CPXnewcols(env, lp, 1, &obj, &lb, &ub, &xctype, colname);
             int numcols = CPXgetnumcols(env, lp);
-            if (numcols != x_pos(i, j, inst->num_nodes)) {
+            if (numcols - 1 != x_pos(i, j, inst->num_nodes)) { // numcols -1 because we need the position index of the new variable
                 print_error("Wrong position of variable");
             }
         }
     }
 
     // Adding the constraints
-    
 
-    // Free as soon as possible the colname array
+    // Saving the model in Lp file
+    char modelPath[1024];
+    sprintf(modelPath, "../model/%s.lp", inst->name);
+    
+    CPXwriteprob(env, lp, modelPath, NULL);
+    
     free(colname[0]);
     free(colname);
 
 }
 
 static int x_pos(int i, int j, int num_nodes) {
-   
 
-   return i * num_nodes + j;
+   return i * num_nodes + j - ((i + 1) * (i + 2)) / 2;
 }
 
-static double distance(int i, int j, instance *inst) {
+static double calc_dist(int i, int j, instance *inst) {
     point node1 = inst->nodes[i];
     point node2 = inst->nodes[j];
     double dx = node1.x - node2.x;
