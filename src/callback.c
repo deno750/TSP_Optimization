c628fdad08cec5225977f4e4a2c3dda3292349c5
deno750 <deronjic.denis@gmail.com>
2021-04-08 16:09:09 +0200

    readded CPXPublic

--- src/callback.c
+++ src/callback.c
@@ -1,5 +1,5 @@
 #include "callback.h"
 
-int SEC_cuts_callback(CPXCALLBACKCONTEXTptr context, CPXLONG contextid, void *userhandle ) {
+int CPXPUBLIC SEC_cuts_callback(CPXCALLBACKCONTEXTptr context, CPXLONG contextid, void *userhandle ) {
     return 0;
 }
