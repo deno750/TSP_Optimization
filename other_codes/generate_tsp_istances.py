055ccece350f000d7cf2f33b8fedf716779e77a6
ivaste <stefano.ivancich@studenti.unipd.it>
2021-04-08 15:18:04 +0200

    Update generate_tsp_istances.py

--- other_codes/generate_tsp_istances.py
+++ other_codes/generate_tsp_istances.py
@@ -3,22 +3,30 @@ import random
 start=1
 stop=1000
 
-N=10
+filename="random_"
 
-filename="nomefile"
-f = open(filename+".tsp", "x")
 
-f.write("NAME : "+filename+"\n")
-f.write("COMMENT : "+str(N)+" random nodes\n")
-f.write("TYPE : TSP\n")
-f.write("DIMENSION : "+str(N)+"\n")
-f.write("EDGE_WEIGHT_TYPE : EUC_2D\n")
-f.write("NODE_COORD_SECTION\n")
+print("insert number of files to create:")
+num_files=int(input())
+print("insert size of the problem")
+N=int(input())
 
-for i in range(1,N+1):
-    x=random.randint(start,stop+1)
-    y=random.randint(start,stop+1)
-    f.write(str(i)+" "+str(x)+" "+str(y)+"\n")
+for nF in range(num_files):
+    f = open(filename+str(nF)+".tsp", "x")
+    f.write("NAME : "+filename+str(nF)+"\n")
+    f.write("COMMENT : "+str(N)+" random nodes\n")
+    f.write("TYPE : TSP\n")
+    f.write("DIMENSION : "+str(N)+"\n")
+    f.write("EDGE_WEIGHT_TYPE : EUC_2D\n")
+    f.write("NODE_COORD_SECTION\n")
 
-f.write("EOF")
-f.close()
+    for i in range(1,N+1):
+        x=random.randint(start,stop+1)
+        y=random.randint(start,stop+1)
+        f.write(str(i)+" "+str(x)+" "+str(y)+"\n")
+
+    f.write("EOF")
+    f.close()
+
+
+print("files created")
