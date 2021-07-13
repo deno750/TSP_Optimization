69d12278eb9ab19c9c41dd68ed3a7d445942be51
ivaste <stefano.ivancich@studenti.unipd.it>
2021-07-13 17:45:14 +0200

    code for plotting solution_cost vs time

---
+++ other_codes/mathHeuristics/plot_cost_time.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import glob
+
+files = glob.glob("*.txt")
+
+time_limit=1200  #in seconds
+
+for file_path in files:
+    f = open(file_path, "r")
+
+    timestamps=[]
+    costs=[]
+    
+    for line in f:
+        if line.startswith("[INFO]  Updated incubement: "):
+            line=line.split()
+            cost=float(line[-1])
+            costs.append(cost)
+        elif line.startswith("[INFO]  Time remaining: "):
+            line=line.split()
+            time=float(line[-2])
+            timestamps.append(time_limit-time)
+        
+    
+    #shift to 0 and convert to second
+    """start=timestamps[0]
+    for i,t in enumerate(timestamps):
+        timestamps[i]-=start
+        timestamps[i]/=1000"""
+    
+    print(len(timestamps),len(costs))
+    plt.plot(timestamps, costs, marker = 'o')
+
+    f.close()
+
+
+
+plt.legend(files)
+plt.xlabel("Time (seconds)")
+plt.ylabel("Solution Cost")
+plt.savefig('plot.png', dpi=600, format='png')
+plt.show()
