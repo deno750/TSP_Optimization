0a5a60e91d6a081348b3f82527446f3e616d47d2
ivaste <stefano.ivancich@studenti.unipd.it>
2021-07-27 18:15:31 +0200

    python plot cost

--- other_codes/metaHeuristics/plot_cost_time.py
+++ other_codes/metaHeuristics/plot_cost_time.py
@@ -14,7 +14,6 @@ for file_path in files:
     for line in f:
         #if line.startswith("[INFO]  incubement: "):
         if line.startswith("[INFO]  Current") or line.startswith("[INFO]  Initial"):
-            print(line)
             line=line.split()
             cost=float(line[-1])
             if cost >565468780: cost=565468780    #565468780 for dsj1000
