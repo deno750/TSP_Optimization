c83af3034178c78b162785dda50d9377fe22fafe
Denis Deronjic <deronjic.denis@gmail.com>
2021-07-30 19:15:12 +0200

    restored the old tabu implementation. Need some code cleaning and fixes but it seems to work

--- src/tabusearch.c
+++ src/tabusearch.c
@@ -62,7 +62,10 @@ static void random_policy(tenure_policy *policy, int curr_iter) {
     } 
 }
 
+// Check whether a node is currently in tabu list or not. If the node has expired the tenure time,
+// the node is removed from tabu list
 int check_tenure(int* node_val, int iter, int tenure) {
+    if (iter < 0 || tenure < 0) { return 0; }
     if (*node_val == 0) return 0;
     if (iter - *node_val > tenure) {
         *node_val = 0;
@@ -105,8 +108,7 @@ int worsening_move(instance *inst, int *skip_node, int iter, int *tabu_size, int
                 (check_tenure(&(skip_node[a]), iter, tenure)  || 
                 check_tenure(&(skip_node[b]), iter, tenure)   || 
                 check_tenure(&(skip_node[a1]), iter, tenure)  || 
-                check_tenure(&(skip_node[b1]), iter, tenure))
-                ) {
+                check_tenure(&(skip_node[b1]), iter, tenure))) {
                 continue;
             }
             double delta = calc_dist(a, b, inst) + calc_dist(a1, b1, inst) - calc_dist(a, a1, inst) - calc_dist(b, b1, inst);
@@ -140,102 +142,164 @@ int worsening_move(instance *inst, int *skip_node, int iter, int *tabu_size, int
 }
 
 
+int alg_2optv2(instance *inst, int *skip_node, int *stored_prev, const int iter, const int tenure) {
+    struct timeval start, end;
+    gettimeofday(&start, 0);
+    double minchange;
+    int status = 0;
+    int *prev = MALLOC(inst->num_nodes, int);
+    MEMSET(prev, -1, inst->num_nodes, int);
+    for (int i = 0; i < inst->num_nodes; i++) {
+        prev[inst->solution.edges[i].j] = i;
+    }
+    int mina = 0;
+    int minb = 0;
+    while(1) {
+        gettimeofday(&end, 0);
+        double elapsed = get_elapsed_time(start, end);
+        if (inst->params.time_limit > 0 && elapsed > inst->params.time_limit) {
+            status = TIME_LIMIT_EXCEEDED;
+            LOG_I("2-opt heuristics time exceeded");
+            break;
+        }
+        minchange = 0;
+        for (int i = 0; i < inst->num_nodes - 1; i++) {
+            if (skip_node && skip_node[i] && check_tenure(&(skip_node[i]), iter, tenure)) { continue; } // Checking here for i can remove an useless cycle
+            for (int j = i+1; j < inst->num_nodes; j++) {
+                int a = i;
+                int b = j;
+                int a1 = inst->solution.edges[a].j;
+                int b1 = inst->solution.edges[b].j;
+                if (skip_node && 
+                    (check_tenure(&(skip_node[a]), iter, tenure)  || 
+                    check_tenure(&(skip_node[b]), iter, tenure)   || 
+                    check_tenure(&(skip_node[a1]), iter, tenure)  || 
+                    check_tenure(&(skip_node[b1]), iter, tenure))) {
+                        continue;
+                    }
+                double change = calc_dist(a, b, inst) + calc_dist(a1, b1, inst) - calc_dist(a, a1, inst) - calc_dist(b, b1, inst);
+                if (change < minchange) {
+                    minchange = change;
+                    mina = i;
+                    minb = j;
+                }
+            }
+        }
+        if (minchange >= 0) {
+            break;
+        }
+        int a1 = inst->solution.edges[mina].j;
+        int b1 = inst->solution.edges[minb].j; 
+        inst->solution.edges[mina].j = minb;
+        inst->solution.edges[a1].j = b1;
+        reverse_path(inst, minb, a1, prev);
+        
+    }
+    inst->solution.obj_best = 0.0;
+    for (int i = 0; i < inst->num_nodes; i++) {
+        edge e = inst->solution.edges[i];
+        inst->solution.obj_best += calc_dist(e.i, e.j, inst);
+    }
+    if(stored_prev) {
+        memcpy(stored_prev, prev, sizeof(int) * inst->num_nodes);
+    }
+    FREE(prev);
+    return status;
+}
 
-
-// The policy parameter 
 static int tabu(instance *inst, void (*policy_ptr)(tenure_policy*, int)) {
     int status = 0;
-
-    //Start counting time from now
     struct timeval start, end;
     gettimeofday(&start, 0);
 
     int *tabu_node = CALLOC(inst->num_nodes, int);
     int *prev = CALLOC(inst->num_nodes, int);
-    int tabu_size = 0;
 
-    //Get initial solution
     int grasp_time_lim = inst->params.time_limit / 5;
     HEU_Greedy_iter(inst);//HEU_Grasp_iter(inst, grasp_time_lim);
-    alg_2opt(inst);
-    if (inst->params.verbose >= 5) {LOG_I("Completed initialization");}
+    if (inst->params.verbose >= 5) {
+        LOG_I("Completed initialization");
+    }
+
     plot_solution(inst);
 
-    //double best_obj = DBL_MAX;
-
-    //Set time limit
-    if (inst->params.time_limit <= 0 && inst->params.verbose >= 3) {LOG_I("Default time lim %d setted.", DEFAULT_TIME_LIM);}
-    int time_limit = inst->params.time_limit > 0 ? inst->params.time_limit : DEFAULT_TIME_LIM;
-
-    double best_obj = inst->solution.obj_best;
+    double best_obj = DBL_MAX;
     edge *best_sol = CALLOC(inst->num_nodes, edge);
-    memcpy(best_sol, inst->solution.edges, inst->num_nodes * sizeof(edge));
     tenure_policy tenure_policy;
-    tenure_policy.min_tenure = inst->num_nodes / 50; // Hyper parameter
-    tenure_policy.max_tenure = inst->num_nodes / 10; // Hyper parameter
-
-    // The following code is useful only with small instances (i.e. less than 200 nodes). Implemented just to avoid infinite loops for those instances or any other error
-    /*if (min_tenure > max_tenure) {
-        min_tenure = max_tenure / 2;
+    tenure_policy.min_tenure = ceil(inst->num_nodes * 0.02);// Ceil in order to have 1 for small instances. //15; // Hyper parameter
+    tenure_policy.max_tenure = round(inst->num_nodes * 0.1);//inst->num_nodes / 10; // Hyper parameter
+    if (tenure_policy.min_tenure == tenure_policy.max_tenure) {
+        tenure_policy.max_tenure += 2;
+    } else if (tenure_policy.max_tenure < tenure_policy.min_tenure) {
+        int tmp = tenure_policy.min_tenure;
+        tenure_policy.min_tenure = tenure_policy.max_tenure;
+        tenure_policy.max_tenure = tmp;
     }
-    if (min_tenure <= 1) {
-        min_tenure = 2;
-    }*/
 
 
     tenure_policy.current_tenure = tenure_policy.min_tenure;
     tenure_policy.incr_tenure = 0;
 
-    int iter = 1; // Starting from 1 in order to have 0 on nodes which are not in tabu list
+    int iter = 1;
     while (1) {
-
-        //Compute elapsed time and stop if exceeded
         gettimeofday(&end, 0);
         double elapsed = get_elapsed_time(start, end);
-        if (elapsed > time_limit) {
+        if (inst->params.time_limit > 0 && elapsed > inst->params.time_limit) {
             status = TIME_LIMIT_EXCEEDED;
             LOG_I("Tabu Search time exceeded");
             break;
         }
+        status = alg_2optv2(inst, tabu_node, prev, iter, tenure_policy.current_tenure);
 
-        // This cycle can be avoided. In the 2opt move when is checking if the node is in tabu list can check whether the iteration is greater than current tenure and update it
-        status = worsening_move(inst, tabu_node, iter, &tabu_size, tenure_policy.current_tenure);
+        if (inst->solution.obj_best < best_obj) {
+            best_obj = inst->solution.obj_best;
+            memcpy(best_sol, inst->solution.edges, inst->num_nodes * sizeof(edge));
+            if (inst->params.verbose >= 3) {
+                LOG_I("Updated incumbent: %f", best_obj);
+            }
+            
+            plot_solution(inst);
+        }
         
         if (status) {
             LOG_I("2-opt move returned status %d", status);
             break;
         }
-        if (tabu_size % tenure_policy.current_tenure == 0) {
-            tabu_size = 0;
-            MEMSET(tabu_node, 0, inst->num_nodes, int);
-            status = alg_2opt(inst);
-        }
-        //LOG_D("Objective: %0.0f Best: %0.0f", inst->solution.obj_best, best_obj);
-        if (inst->solution.obj_best < best_obj) {
-            best_obj = inst->solution.obj_best;
-            memcpy(best_sol, inst->solution.edges, inst->num_nodes * sizeof(edge));
-            if (inst->params.verbose >= 3) {
-                LOG_I("Updated incubement: %0.0f", best_obj);
+        // We're in local minimum now. We have to swap two edges and add a node in the tabu list
+
+        int a = 0, b = 0, a1 = 0, b1 = 0; 
+        // Seeking the pair edges to change. We don't want to choose two adiacent edges to swap
+        while (a == b || a1 == b1 || a == a1 || a == b1 || b == a1 || b == b1) {
+            a = rand_choice(0, inst->num_nodes);
+            b = rand_choice(0, inst->num_nodes);
+            a1 = inst->solution.edges[a].j;
+            b1 = inst->solution.edges[b].j;
+            if (tabu_node[a] || tabu_node[b] || tabu_node[a1] || tabu_node[b1]) {
+                a = 0; b = 0; a1 = 0; b1 = 0;
             }
-            
-            plot_solution(inst);
         }
+        inst->solution.edges[a].j = b;
+        inst->solution.edges[a1].j = b1;
+        reverse_path(inst, b, a1, prev);
 
         (*policy_ptr)(&tenure_policy, iter);
 
         if (inst->params.verbose >= 5) {
             LOG_I("Current tenure %d", tenure_policy.current_tenure);
         }
+        //printf("\n\n\n\n");
+        //plot_solution(inst);
+        //sleep(1);
+        
+        int node_fixed = random_choice(a, b, a1, b1);
+        tabu_node[node_fixed] = iter;
         
         iter++;
     }
 
-    //Update tour cost
     inst->solution.obj_best = best_obj;
     memcpy(inst->solution.edges, best_sol, inst->num_nodes * sizeof(edge));
-
-    status = alg_2opt(inst); // A very fast 2-opt which removes the remaining crossing edges (generally few crossing edges)
-
+    status = alg_2optv2(inst, NULL, NULL, -1, -1); // A very fast 2-opt which removes the remaining crossing edges (generally few crossing edges)
     FREE(tabu_node);
     FREE(prev);
     return status;
